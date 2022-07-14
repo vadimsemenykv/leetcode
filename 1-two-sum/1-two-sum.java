d7f836d6596f9832af5285c7673bfb88d2c19077
vadimsemenykv <vadimsemenykv@users.noreply.github.com>
2022-07-14 23:36:19 +0300

    Time: 106 ms (14.89%), Space: 45.1 MB (55.22%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,19 +1,29 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // O(n) time | O(n) space
-        HashMap<Integer, Integer> myMap = new HashMap<>();
+        int first=0;
+        int second=1;
         
-        // update map
-        for(int i = 0; i < nums.length; i++)
-            myMap.put(nums[i], i);
-        
-        for(int i = 0; i < nums.length; i++)
-        {
-            int found = target - nums[i];
+        int[] res=new int[2];
+        boolean found=false;
+        int sum=0;
+                
+        while(!found){
+            if(second == nums.length){
+                first++;
+                second=first+1;
+            }
             
-            if(myMap.containsKey(found) && i != myMap.get(found))
-                return new int[] {i, myMap.get(found)};
+            sum=nums[first]+nums[second];
+            
+            if(sum==target){
+                res[0]=first;
+                res[1]=second;
+                found=true;
+            }
+            sum=0;
+            second++;
         }
-        throw new IllegalArgumentException("No Solution !");
+        
+        return res;
     }
 }
