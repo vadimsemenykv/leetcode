4793ad88c35064ef52f6fbd23b6909b207768097
vadimsemenykv <vadimsemenykv@users.noreply.github.com>
2022-07-17 16:42:51 +0300

    Time: 27 ms (27.00%), Space: 41.2 MB (26.56%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.java
@@ -0,0 +1,41 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        //we will use binary search lower_bound variant
+        int start = 0;
+        int end = n;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    
+    //Example n = 7  [1, 2, 3, 4, 5, 6, 7], 3 is bad
+    // CH - Condition check, I{i} - Iteration {i}
+    //Iterations:
+    //  CH: start(0) <= end(7) TRUE
+    //  I1: start = 0, end = 7, mid = 4
+    //      isBad... = true
+    //      end = 4 - 1 = 3
+    //  CH: start(0) <= end(3) TRUE
+    //  I2: start = 0, end = 3, mid = 0 + (3 - 0) / 2 = 1
+    //      isBad... = false
+    //      start = 1 + 1 = 2
+    //  CH: start(2) <= end(3) TRUE
+    //  I2: start = 2, end = 3, mid = 2 + (3 - 2) / 2 = 2
+    //      isBad... = false
+    //      end = 2 + 1 = 3;
+    //  CH: start(3) <= end(3) TRUE
+    //  I2: start = 3, end = 3, mid = 3 + (3 - 3) / 2 = 3
+    //      isBad... = true
+    //      end = 3 - 1 = 2;
+    //  CH: start(3) <= end(2) FALSE
+    //  return start 3
+}
