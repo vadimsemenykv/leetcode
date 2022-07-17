c2b833322804d5a3678ff80c9c142b74754fe603
vadimsemenykv <vadimsemenykv@users.noreply.github.com>
2022-07-17 16:48:08 +0300

    Time: 32 ms (13.99%), Space: 40 MB (83.27%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -16,10 +16,10 @@ public class Solution extends VersionControl {
         
         int start = 0;
         int end = n;
-        while (start < end) {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
             if (isBadVersion(mid)) {
-                end = mid;
+                end = mid - 1;
             } else {
                 start = mid + 1;
             }
