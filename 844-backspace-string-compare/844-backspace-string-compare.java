7306455c38ea68bb7721d1b8010ac175c1c97118
Vadym <vadimsemenykv@users.noreply.github.com>
2022-07-19 19:01:37 +0300

    Time: 1 ms (93.96%), Space: 42.6 MB (25.17%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,8 +1,38 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return process(s).equals(process(t));
+        int p1 = s.length() - 1;
+        int p2 = t.length() - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            if (getCharSafely(s, p1) == '#' || getCharSafely(t, p2) == '#') {
+                int backCount = 2;
+                if (getCharSafely(s, p1) == '#') {
+                    while (backCount > 0) {
+                        p1--;
+                        backCount--;
+                        if (getCharSafely(s, p1) == '#') backCount += 2;
+                    }
+                } else {
+                    while (backCount > 0) {
+                        p2--;
+                        backCount--;
+                        if (getCharSafely(t, p2) == '#') backCount += 2;
+                    }
+                }
+            } else {
+                if (getCharSafely(s, p1) != getCharSafely(t, p2)) return false;
+                p1--;
+                p2--;
+            }
+        }
+
+        return true;
+        // return process(s).equals(process(t));
     }
     
+    private Character getCharSafely(String s, int p) {
+        return p >= 0 ? s.charAt(p) : Character.MIN_VALUE; 
+    }
+        
     private List<Character> process(String s) {
         List<Character> chars = new LinkedList<>();
         for (int i = 0; i < s.length(); i++) {
