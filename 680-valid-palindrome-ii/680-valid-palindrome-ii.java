c7a2b97561518229fb38223a67b0db4fb16b1190
Vadym <vadimsemenykv@users.noreply.github.com>
2022-08-08 13:26:44 +0300

    Time: 14 ms (34.41%), Space: 53.9 MB (81.01%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -2,28 +2,33 @@ class Solution {
     public boolean validPalindrome(String s) {
         var pL = 0;
         var pR = s.length() - 1;
+        var toRemove = 0;
+        var stepsToReturn = 0;
+        var checkLeftBranch = false;
         
         while (pL < pR) {
             if (s.charAt(pL) != s.charAt(pR)) {
-                return isSubStringValidPalindrome(s, pL + 1, pR)
-                    || isSubStringValidPalindrome(s, pL, pR - 1);
+                if (checkLeftBranch) {
+                    pL = pL - stepsToReturn - 1;
+                    pR = pR + stepsToReturn - 1;
+                    stepsToReturn = 0;
+                    checkLeftBranch = false;
+                } else {
+                    pL++;
+                    checkLeftBranch = true;
+                    toRemove++;
+                }
+            } else {
+                pL++;
+                pR--;
+                if (checkLeftBranch) {
+                    stepsToReturn++;
+                }
             }
             
-            pL++;
-            pR--;
-        }
-        
-        return true;
-    }
-    
-    private boolean isSubStringValidPalindrome (String s, int pL, int pR) {
-        while (pL < pR) {
-            if (s.charAt(pL) != s.charAt(pR)) {
+            if (toRemove > 1) {
                 return false;
-            } 
-            
-            pL++;
-            pR--;
+            }
         }
         
         return true;
