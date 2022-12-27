f0d596dd4983415f0fbf7f44a5be222b4adb0175
Vadym <vadimsemenykv@users.noreply.github.com>
2022-12-27 11:56:26 +0200

    Time: 0 ms (100.00%), Space: 42.9 MB (79.97%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -11,21 +11,6 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-//         if (head == null || head.next == null) {
-//             return null;
-//         }
-        
-//         var current = head;
-//         Set<ListNode> checkedNodes = new HashSet();
-
-        
-//         while (!checkedNodes.contains(current) && current != null) {
-//             checkedNodes.add(current);
-//             current = current.next;
-//         }
-        
-//         return current;
-        
         var tortoise = head;
         var hare = head;
         boolean met = false;
