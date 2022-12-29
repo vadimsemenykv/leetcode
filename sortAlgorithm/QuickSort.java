d0bf81ea40c89f463991a2b6d4af371c9d3b4926
Vadym <vadimsemenykv@users.noreply.github.com>
2022-12-29 23:30:04 +0200

    Update QuickSort.java

--- sortAlgorithm/QuickSort.java
+++ sortAlgorithm/QuickSort.java
@@ -4,32 +4,32 @@ class QuickSort {
   }
   
   private void sort(int[] nums, int low, int high) {
-        if (low < high) {
-            var pivotIndex = partition(nums, low, high);
-            
-            sort(nums, low, pivotIndex - 1);
-            sort(nums, pivotIndex + 1, high);
-        }
+    if (low < high) {
+      var pivotIndex = partition(nums, low, high);
+
+      sort(nums, low, pivotIndex - 1);
+      sort(nums, pivotIndex + 1, high);
     }
+  }
     
-    private int partition(int[] nums, int low, int high) {
-        var pivot = nums[high];
-        var i = low - 1;
-        
-        for (var j = low; j < high; j++) {
-            if (nums[j] < pivot) {
-                i++;
-                swap(nums, i, j);
-            }
-        }
-        swap(nums, i + 1, high);
-        
-        return i + 1;
+  private int partition(int[] nums, int low, int high) {
+    var pivot = nums[high];
+    var i = low - 1;
+
+    for (var j = low; j < high; j++) {
+      if (nums[j] < pivot) {
+        i++;
+        swap(nums, i, j);
+      }
     }
-    
-    private void swap(int[] nums, int i, int j) {
-        var temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
+    swap(nums, i + 1, high);
+
+    return i + 1;
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    var temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
 }
